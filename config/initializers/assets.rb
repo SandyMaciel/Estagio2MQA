4286354158b6885ee8f89f7b2232d6b408b80384
SandyMaciel <elleysandymaciel@gmail.com>
2015-11-22 12:24:27 -0300

    #1 Adicionando libs do bootstrap gerenciados via rails para corrigir a remocao.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,6 +8,12 @@ Rails.application.config.assets.precompile += %w( jquery.prettyPhoto.js )
 Rails.application.config.assets.precompile += %w( jquery.isotope.min.js )
 Rails.application.config.assets.precompile += %w( main.js )
 Rails.application.config.assets.precompile += %w( wow.min.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( font-awesome.min.css )
+Rails.application.config.assets.precompile += %w( animate.min.css )
+Rails.application.config.assets.precompile += %w( prettyPhoto.css )
+Rails.application.config.assets.precompile += %w( main.css )
+Rails.application.config.assets.precompile += %w( responsive.css )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
